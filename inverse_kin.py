3d81266643e6e49c8f019e3ecad9d149753c52f5
akiran709 <kirankashok@gmail.com>
2025-06-25 12:32:43 -0700

    setup env and defined config files

--- inverse_kin.py
+++ inverse_kin.py
@@ -7,8 +7,39 @@ import os
 
 from lerobot.common.robot_devices.motors.feetech import FeetechMotorsBus
 from lerobot.common.robot_devices.motors.configs import FeetechMotorsBusConfig
-from config import robot_config
+from config_robot import robot_config
 import time
 from camera_controller import CameraController
 import traceback
 from dataclasses import dataclass, field
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+logger = logging.getLogger(__name__) # Use a named logger
+
+class output_movement:
+    #typing hinting some variables
+    warnings: List[str] = field(default_factory=list)
+    robot_state: Dict[str, Any] = field(default_factory=dict)
+    ok : bool
+    msg: str
+    
+    def convert_to_json(self) -> dict:
+        final_state_robot = self.robot_state
+        if not final_state_robot:
+            final_state_robot = {"error" : "robot not avaliable"}
+        output_json = {}
+        
+        #this is to make sure we are not sending empty values to the calude model
+        if not self.ok:
+            output_json["status"]  = "error"
+        if self.robot_state:
+            output_json["robot_state"]  = self.robot_state
+        if self.warnings:
+            output_json["warning"]  = self.warnings
+        if self.msg:
+            output_json["message"] = self.msg
+        
+        
+         # Single point of logging for the returned JSON
+        logger.info(f"MoveResult JSON: {json.dumps(output_json)}")
+        return output_json
